35cf0d5cc92751d02b38ed5c2c6312292d69d0a6
cameron <Dcameroncheng@gmail.com>
2020-05-26 02:26:12 +0000

    complete exercise 2 and 3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.update(name: 'coquitlam')
+
+@store2 = Store.find_by(id: 2)
+
+puts Store.count
