f4c1a0d9d0240e333a3de666fd5e25e1409f5d6c
cameron <Dcameroncheng@gmail.com>
2020-05-26 03:36:36 +0000

    complete ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@all_revenue = Store.sum("annual_revenue") 
+puts @all_revenue
+@all_revenue_avg = Store.average("annual_revenue")
+puts @all_revenue_avg
+
+@one_m_or_more = Store.where("annual_revenue > ?", 1000000).count
+puts @one_m_or_more
